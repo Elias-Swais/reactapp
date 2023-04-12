69beaa305a5703ff187433cbc20a8cbb67dde50f
elias <elias.al-swais@pwc.com>
2023-04-12 11:07:58 +0300

    added api call

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,43 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import MainContent from "../components/MainContent";
+import axios, { AxiosResponse } from 'axios';
+import { useEffect, useState } from "react";
+const Axios = axios.defaults;
+
+interface postProps{
+  title:string
+  price:string
+  products: []
+}
+
+ const baseURL = "https://fakestoreapi.com/products/1"
+
 
 export function Home() {
+  const [post,setPost] = useState<postProps>();
+  
+  
+  useEffect(()=>{
+    axios.get(baseURL).then((response) =>{
+      setPost(response.data)
+    })
+  },[]);
+
+  if(!post) return "No Products!"
   return (
     <>
       <Header />
       <MainContent/>
+      <div>
+        <h3>Get Products</h3>
+        <ul>
+          <li>
+            <h4>{post.title}</h4>
+            <p>{post.price}</p>
+          </li>
+        </ul>
+      </div>
       <Footer />
     </>
   );
